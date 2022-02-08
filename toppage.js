396da808d767e2fdb5ee44d9d516ad3750505f62
kazuki663 <kazkaz.t@icloud.com>
2022-02-08 15:00:30 +0900

    pull前

--- toppage.js
+++ toppage.js
@@ -49,3 +49,43 @@ const mySwiper = new Swiper('.swiper-container', {
     }
   },
 });
+// ここまで
+
+// ここから画面遷移
+$(window).on('load',function(){
+  
+  
+  $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+  
+  });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  
+  //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+  $('.splashbg').on('animationend', function() { 
+  //この中に動かしたいJSを記載
+  });
+  
+
+  // ここまで
+
+  // ここから画面遷移スクロール
+  function fadeAnime(){
+
+    // ふわっ
+    $('.h1').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('splashbg');// 画面内に入ったらfadeUpというクラス名を追記
+      }else{
+      $(this).removeClass('splashbg');// 画面外に出たらfadeUpというクラス名を外す
+      }
+      });
+    }
+    
+    // 画面をスクロールをしたら動かしたい場合の記述
+      $(window).scroll(function (){
+        fadeAnime();/* アニメーション用の関数を呼ぶ*/
+      });// ここまで画面をスクロールをしたら動かしたい場合の記述
+// ここまで
